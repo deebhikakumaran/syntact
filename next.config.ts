3ffc7ffaf40163f4bd2baab72890bacd024b2060
deebhikakumaran <deebhika04@gmail.com>
2026-06-26 18:13:44 +0530

    fix: origin changed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['crying-compound-elixir.ngrok-free.dev'],
+  images: {
+      remotePatterns: [
+          { protocol: "https", hostname: "avatars.githubusercontent.com" },
+      ],
+  },
+  allowedDevOrigins: ['codefox-a7ft.vercel.app'],
 };
 
 export default nextConfig;
